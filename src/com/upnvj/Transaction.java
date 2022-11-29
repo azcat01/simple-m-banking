965a7609a4f7adda7661b5c14420f1c1d2db3a57
Irsyad <azcat01@yahoo.co.id>
2022-11-29 16:05:28 +0700

    minor changes

--- src/com/upnvj/Transaction.java
+++ src/com/upnvj/Transaction.java
@@ -1,4 +1,12 @@
 package com.upnvj;
 
+import java.util.*;
+
 public class Transaction {
+  private static int idTransaction;
+
+  public Date getDate() {
+    Date date = new Date();
+    return date;
   }
+}
