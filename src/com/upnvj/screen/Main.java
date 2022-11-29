c87b950194e029a7ee5930350dacdf638f76e91e
oxidices <ndaffaraihan@gmail.com>
2022-11-29 14:19:48 +0700

    added GUI for splash screen, sign in, sign up, and dashboard

---
+++ src/com/upnvj/screen/Main.java
@@ -0,0 +1,8 @@
+package com.upnvj.screen;
+
+public class Main {
+    public static void main(String[] args) {
+        Screen_SplashScreen start = new Screen_SplashScreen();
+        start.setVisible(true);
+    }
+}
